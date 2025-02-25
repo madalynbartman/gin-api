26e10072854d39048ddee6964ed59ea0d0e32409
Madalyn <madalynbartman@gmail.com>
2025-02-24 22:20:18 -0700

    Add POST endpoint and finish GET endpoint

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+// will automatically import after you save
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 // create a struct to store the books for our library
 
 type book struct {
@@ -17,4 +24,30 @@ var books = []book{
 	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
 	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
 	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
+}
+
+// gin context is all the info about the request and allows you to return a response
+func getBooks(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, books) // sending http response code, then the books as JSON with the proper indentation. S
+	// serializes the book struct and sends it as a json object.
+	// can return files or other artifacts, doesnt need to be json.
+	// test with curl localhost:8080/books
+}
+
+func createBook(c *gin.Context) {
+	var newBook book
+
+	if err := c.BindJSON(&newBook); err != nil {
+		return //if you dont return, youll bind the json to the newBook return
+	}
+
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/books", getBooks)
+	router.POST("/books", createBook)
+	router.Run("localhost:8080")
 }
